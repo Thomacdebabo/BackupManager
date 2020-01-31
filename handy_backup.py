794ea29796a74e28d59db09a7f9283314155f9e1
thomacdebabo <thomas.rueegg98@bluewin.ch>
2020-01-31 12:13:39 +0100

    cleaned up

--- handy_backup.py
+++
@@ -1,6 +0,0 @@
-import Backup as B
-import os
-BM = B.BackupManager(r"C:\Users\Thomacdebabo\Desktop\Backups\SamsungA6")
-BM.newBackup("SamsungA6Bilder", r"Dieser PC\Galaxy A6+\Phone\DCIM")
-BM.backupALL()
-BM.close()
